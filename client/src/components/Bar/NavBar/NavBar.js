5c7766b429b7983c07e43de90f3e445519b7e40c
numwhanwnp <whan.sweetwater@gmail.com>
2020-03-28 21:48:17 +0700

    update NavBar

--- client/src/components/Bar/NavBar/NavBar.js
+++ client/src/components/Bar/NavBar/NavBar.js
@@ -32,25 +32,24 @@ class NavBar extends Component {
 
         return (
             <div>
+                <nav class="navbar navbar-default navbar-fixed-top">
                 <div className="navHeader">
                     <a href="/"><img className ="photo" src="https://sv1.picz.in.th/images/2020/02/14/xK1HvZ.png" alt="xK1HvZ.png" border="0" width="auto" height="32" /></a>
-                    <a id="home">หน้าหลัก <i class="fas fa-home"></i></a>
-                    <a id="review">รีวิวนิยาย <i class="fas fa-edit"></i></a>
-                    <a id="broad">กระทู้ <i class="fas fa-comments"></i></a>
-                    <a id="saved">เก็บไว้อ่าน <i class="fas fa-heart"></i></a>
+                    <a id="home">หน้าหลัก <i className="fas fa-home"></i></a>
+                    <a id="review">รีวิวนิยาย <i className="fas fa-edit"></i></a>
+                    <a id="broad">กระทู้ <i className="fas fa-comments"></i></a>
+                    <a id="saved">เก็บไว้อ่าน <i className="fas fa-heart"></i></a>
 
-                    <a id="login"><i class="fas fa-user"> เข้าสู่ระบบ</i></a>
-                    <span class="search">  <i class="fas fa-search"></i> ค้นหารีวิวนิยาย  </span>
-
-                    {/*<a href="/"><div id= "search"><p><i  className="fas fa-search" id="search"/></p></div></a>*/}
-                    {/*<a href="/login"><div id= "search"><p><i className="fas fa-user" id="search"/></p></div></a>*/}
-                    {/* { isAuthenticated ? memberLinks : guestLinks } */}
+                    <a id="box">
+                    <a id="login"><i className="fas fa-user"> เข้าสู่ระบบ</i></a>
+                    <span class="search">  <i className="fas fa-search"></i> ค้นหารีวิวนิยาย  </span>
+                    </a>
                 </div>
+                </nav>
             </div>   
         );
     }
 }
-
 const mapStateToProps = state => {
     return {
         user: state.user
